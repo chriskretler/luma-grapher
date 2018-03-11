84119c7bf9e71e46c3da2c9f45a4642a7a38416f
Chris <ckretler@umich.edu>
2018-03-11 15:45:26 -0400

    improve algorithm to find first value above 30, rather than the max value in the range to scan.

--- undershoot.py
+++ undershoot.py
@@ -5,12 +5,20 @@ for i in range(0, len(array)):
 
    # If a luma value is less than 30, enter the fixing loop.
    if array[i] < 30:
-      max_value = max(array[i:i+8])
-      max_index = array.index(max_value)
-
-      # set a floor of 30 as min_value
-      if max_value < 30:
-         max_value = 30
+      max_index = 0
+      max_value = 0
+      while max_value == 0:
+         # iterate through defined number of pixels looking for a value > 30.
+         # if you find one, break out of the array
+         for v in array[i:i+6]:
+            if v > 30:
+               max_index = array.index(v)
+               max_value = v
+               break
+         # set a floor of 30 as min_value
+         if max_value < 30:
+            max_value = 30
+            max_index = i+6
 
       # assign all values from i to max_index = max_value
       for j in range(i, max_index):
