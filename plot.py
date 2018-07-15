d7c036d23a0443c14ee7e4aacb8c60e555dc5e6c
Chris <ckretler@umich.edu>
2018-07-15 16:32:40 -0400

    separate identify and fix functions. First fix is via averaging.

--- plot.py
+++ plot.py
@@ -1,19 +1,26 @@
-import matplotlib.pyplot as plt
+#!/usr/bin/env python
 import sys
+import matplotlib.pyplot as plt
+from numpy import genfromtxt
 
 file = sys.argv[1]
 
 with open(file) as f:
    array = map(int, f)
 
-# develop basic plot
+# Plot input variable as primary graph.
 plt.plot(array)
+
+# Also plot list of points of identified overshoot.
+overshoot_array = genfromtxt('overshoot_points.txt', delimiter=',')
+
+# First variable is x coordinate, second is original luma value.
+plt.plot(overshoot_array[:,0], overshoot_array[:,1], 'o')
 
 # adjust axis, first two vals are x, second two are y.
 plt.axis([0, len(array)-1, 0, 255])
 
-# sample legend
+# Sample Legend
 plt.ylabel('Luma Values')
 
-# display
 plt.show()
